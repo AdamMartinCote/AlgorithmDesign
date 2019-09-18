a0b26af29fc3578eb6c26c9a943f24d120ce60b4
Adam Martin-Côté <adammartincote@gmail.com>
2019-09-18 10:13:49 -0400

    add bruteforce from github

--- code/algorithms/bruteforce.py
+++ code/algorithms/bruteforce.py
@@ -1,12 +1,13 @@
 import time
-from utils import MAX_DIST, distance
+from .utils import MAX_DIST, distance
+
 
 def brute_force(points):
     """ Si le nombre de points est suffisamment petit, on préfère utiliser cet algorithme.
     """
     dist_min = MAX_DIST
     for pos, pt1 in enumerate(points):
-        for pt2 in points[(pos+1):]:
+        for pt2 in points[(pos + 1):]:
             tmp_dist = distance(pt1, pt2)
             dist_min = min(tmp_dist, dist_min)
     return dist_min
@@ -17,4 +18,4 @@ def execute_brute_force(points):
     min_brute_force = brute_force(points)
     end = time.time()
     # print("BF: ", min_brute_force)
-    return end-start
+    return end - start
--- code/tp1.py
+++ code/tp1.py
@@ -4,14 +4,15 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--algorithm",
-                        help="algorithm to use")
-    parser.add_argument("-e", "--ex_path",
-                        help="")
+    parser.add_argument("-a", "--algorithme",
+                        choices=['brute', 'seuil'],
+                        help="algorithme utilisé")
+    parser.add_argument("-e", "--path_vers_exemplaire",
+                        help="path to exemplaire")
     parser.add_argument("-p", "--show_p", action='store_true',
-                        help="")
+                        help="affiche la plus petite distance entre deux points, sans texte superflu")
     parser.add_argument("-t", "--show_t", action='store_true',
-                        help="")
+                        help="affiche le temps d’exécution en ms, sans unité ni texte superflu")
     args = parser.parse_args()
 
     print(args)
