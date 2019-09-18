7375717d259412291a4f636a15ce8386256726c3
Adam Martin-Côté <adammartincote@gmail.com>
2019-09-18 10:04:20 -0400

    add caller script

--- code/tp1.py
+++ code/tp1.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
 
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--algorithm",
+                        help="algorithm to use")
+    parser.add_argument("-e", "--ex_path",
+                        help="")
+    parser.add_argument("-p", "--show_p", action='store_true',
+                        help="")
+    parser.add_argument("-t", "--show_t", action='store_true',
+                        help="")
+    args = parser.parse_args()
+
+    print(args)
