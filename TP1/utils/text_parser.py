1a320ea4a7f1b0b0b268deadb501e88b47fb765f
Vincent Hogues <vincent.hogues@polymtl.ca>
2019-09-18 11:04:41 -0400

    class point + text file parser

---
+++ TP1/utils/text_parser.py
@@ -0,0 +1,17 @@
+import point 
+import numpy as np
+
+def txt_to_array(file_path):
+    f = open(file_path,"r");f.readline() #Skip first line 
+    points = []
+    for l in f:
+        line = l.split()
+        x    = int(line[0])
+        y    = int(line[1])
+        pt = point.Point(x,y)
+        points.append(pt)
+    return points
+# file_path = input("Your file path: ")
+points = txt_to_array("./../code/tmp.txt")
+for p in points:
+    print(p.x,p.y)
