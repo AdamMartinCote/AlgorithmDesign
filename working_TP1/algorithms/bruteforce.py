62a5cb5a1406e48b3985b3e39c491e1f1dd58e44
Vincent Hogues <vincent.hogues@polymtl.ca>
2019-10-15 19:36:02 -0400

    git issues solving try 1

---
+++ working_TP1/algorithms/bruteforce.py
@@ -0,0 +1,24 @@
+import time
+from typing import List
+
+from .common import MAX_DIST, distance
+
+
+def brute_force(points: List[List[int]]) -> float:
+    """ Si le nombre de points est suffisamment petit, on préfère utiliser cet algorithme.
+    """
+    dist_min = MAX_DIST
+    for pos, pt1 in enumerate(points):
+        for pt2 in points[(pos + 1):]:
+            tmp_dist = distance(pt1, pt2)
+            dist_min = min(tmp_dist, dist_min)
+    return dist_min
+
+
+def execute_brute_force(points) -> float:
+    start = time.time()
+    min_brute_force = brute_force(points)
+    end = time.time()
+    # print("BF: ", min_brute_force)
+    return end - start
+
---
+++ working_TP1/tp1.py
@@ -0,0 +1,28 @@
+import argparse
+
+from algorithms.bruteforce import brute_force
+from algorithms.seuil import seuil
+import code.profiling.profile_script as profiler
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--algorithme",
+                    required=True,
+                    choices=['brute', 'seuil','profile'],
+                    help="algorithme utilisé")
+parser.add_argument("-e", "--path_vers_exemplaire",
+                    help="path to exemplaire")
+parser.add_argument("-p", "--show_p", action='store_true',
+                    help="affiche la plus petite distance entre deux points, sans texte superflu")
+parser.add_argument("-t", "--show_t", action='store_true',
+                    help="affiche le temps d’exécution en ms, sans unité ni texte superflu")
+args = parser.parse_args()
+
+if args.algorithme == 'brute':
+    val = brute_force([1,2])
+elif args.algorithme == 'seuil':
+    val = seuil([[1, 2], [3, 4]], [[2, 3], [4, 5]], 1)
+    print(val)
+
+elif args.algorithme == 'profile':
+    profiler.collectResults()
