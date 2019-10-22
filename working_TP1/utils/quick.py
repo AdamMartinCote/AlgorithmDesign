78bca1a37b6ebc99c253ac46f13da93c63f73bf1
Adam Martin-Côté <adammartincote@gmail.com>
2019-10-22 09:59:40 -0400

    PEP8

--- working_TP1/utils/quick.py
+++ working_TP1/utils/quick.py
@@ -1,29 +1,31 @@
 import numpy as np
 
+
 def quicksort(table, first, last, shift):
-        if first < last:
-                z = partition(table, first, last,shift)
-                quicksort(table, first, z - 1,shift)
-                quicksort(table, z + 1, last,shift)
-        return table
+    if first < last:
+        z = partition(table, first, last, shift)
+        quicksort(table, first, z - 1, shift)
+        quicksort(table, z + 1, last, shift)
+    return table
+
 
 def partition(table, first, last, shift):
-        pivot = table[first][shift]
-        i     = first + 1
-        j     = last
-        done  = False
-        while not done:
-                while i <= j and table[i][shift] <= pivot:
-                        i += 1
-                while j >= i  and table[j][shift] >= pivot:
-                        j -= 1
-                if j < i:
-                        done = True
-                else:
-                        tmp      = table[j]
-                        table[j] = table[i]
-                        table[i] = tmp
-        tmp          = table[first]
-        table[first] = table[j]
-        table[j]     = tmp
-        return j
+    pivot = table[first][shift]
+    i = first + 1
+    j = last
+    done = False
+    while not done:
+        while i <= j and table[i][shift] <= pivot:
+            i += 1
+        while j >= i and table[j][shift] >= pivot:
+            j -= 1
+        if j < i:
+            done = True
+        else:
+            tmp = table[j]
+            table[j] = table[i]
+            table[i] = tmp
+    tmp = table[first]
+    table[first] = table[j]
+    table[j] = tmp
+    return j
--- working_TP1/utils/text_parser.py
+++ working_TP1/utils/text_parser.py
@@ -1,36 +1,38 @@
 from utils.quick import quicksort
 import numpy as np
 
+
 def fileToArrayBruteMethod(file_path):
-    f = open(file_path,"r")
-    f.readline()                #Skip first line 
+    f = open(file_path, "r")
+    f.readline()  # Skip first line
     points = []
     try:
         for l in f:
             line = l.split()
-            x    = int(line[0])
-            y    = int(line[1])
-            points.append([x,y])
+            x = int(line[0])
+            y = int(line[1])
+            points.append([x, y])
     except:
         raise ValueError()
     f.close()
     return points
 
+
 def fileToArraySeuilMethod(file_path):
-    f = open(file_path,"r")
-    f.readline()                #Skip first line 
+    f = open(file_path, "r")
+    f.readline()  # Skip first line
     xSortedPoints = []
     ySortedPoints = []
-    points        = []
+    points = []
     try:
         for l in f:
             line = l.split()
-            x    = int(line[0])
-            y    = int(line[1])
-            points.append([x,y])
-        xSortedPoints = quicksort(points,0,len(points)-1,0)
-        ySortedPoints = quicksort(points,0,len(points)-1,1)
+            x = int(line[0])
+            y = int(line[1])
+            points.append([x, y])
+        xSortedPoints = quicksort(points, 0, len(points) - 1, 0)
+        ySortedPoints = quicksort(points, 0, len(points) - 1, 1)
     except:
         raise ValueError()
     f.close()
-    return xSortedPoints,ySortedPoints
+    return xSortedPoints, ySortedPoints
