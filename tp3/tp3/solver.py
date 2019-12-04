0bef8dcd1b649062fa2406ee2fdf1429ca828a07
Adam Martin-Côté <adammartincote@gmail.com>
2019-12-04 18:16:33 -0500

    implement build_by_layer

--- tp3/tp3/solver.py
+++ tp3/tp3/solver.py
@@ -14,8 +14,8 @@ class Solver:
         self.exemplaire = exemplaire
 
     def __call__(self) -> Solution:
-        return self.build_deck_in_order()
-        # return self.build_decks_by_layer()
+        # return self.build_deck_in_order()
+        return self.build_decks_by_layer()
 
     def generate_pairs(self, cards: List[Card]) -> List[Pair]:
         return [Pair(*pair) for pair in combinations(cards, 2)]
@@ -27,12 +27,6 @@ class Solver:
             synergy += exemplaire.get_synergy_by_idx(pair[0].idx, pair[1].idx)
         return synergy
 
-    def get_worst_pair(self, cards: List[Card]):
-        pairs = self.generate_pairs(cards)
-        worst: Tuple[Card, Card] = pairs[0]
-        for pair in pairs[1:]:
-            pass
-
     def evaluate_pair_value(self, id1: int, id2: int):
         return self.exemplaire.baseQuality[id1] + \
                self.exemplaire.baseQuality[id2] + \
@@ -43,12 +37,20 @@ class Solver:
                       base_value=self.exemplaire.baseQuality[i],
                       synergy=self.exemplaire.synergyMatrix[i])
                  for i in range(self.exemplaire.nCards * self.exemplaire.nDecks)]
-        pairs = [Pair(cards[i], cards[i + 1]) for i in range(len(cards) - 1)]
+        pairs = [Pair(cards[i], cards[i + 1]) for i in range(0, len(cards) - 1, 2)]
         sorted_pairs = sorted(pairs, key=lambda x: x.get_value())
 
         decks = [Deck() for _ in range(self.exemplaire.nDecks)]
+        skip = 0
         for i, pair in enumerate(sorted_pairs):
-            decks[i % self.exemplaire.nDecks].cards += pair.cards
+            insert_index = i + skip
+            deck = decks[insert_index % self.exemplaire.nDecks]
+            deck.cards.append(pair[0])
+            if len(deck.cards) < self.exemplaire.nCards:
+                deck.cards.append(pair[1])
+            else:
+                decks[(insert_index % self.exemplaire.nDecks) + 1].cards.append(pair[1])
+                skip += 1
 
         solution = Solution()
         solution.decks = decks
@@ -63,6 +65,6 @@ class Solver:
                 deck.cards.append(Card(idx=flat_idx,
                                        base_value=self.exemplaire.baseQuality[flat_idx],
                                        synergy=self.exemplaire.synergyMatrix[flat_idx]))
-            print(f'deck synergy: {self.evaluate_deck_synergy(deck, self.exemplaire)}')
+            # print(f'deck synergy: {self.evaluate_deck_synergy(deck, self.exemplaire)}')
             solution.decks.append(deepcopy(deck))
         return solution
