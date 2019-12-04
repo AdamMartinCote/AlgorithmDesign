9e04eb9df21c6d632382007c53f7c4d4896f2f6a
Adam Martin-Côté <adammartincote@gmail.com>
2019-12-04 13:56:57 -0500

    add high-level runner

--- tp3/solve_and_check.sh
+++ tp3/solve_and_check.sh
@@ -1,5 +1,20 @@
-exemplaire='./exemplaires/MTG_10_10'
-solution='./tmp/sol_10_10'
-bash tp.sh -e $exemplaire > $solution
-python3 ./sol_check.py $exemplaire $solution
+#!/usr/bin/env bash
 
+function check_exemplaire() {
+    echo -e "Résultats pour MTG_$1: "
+    bash tp.sh -e "./exemplaires/MTG_$1" >"./tmp/sol_$1"
+    python3 ./sol_check.py "./exemplaires/MTG_$1" "./tmp/sol_$1"
+    echo ""
+}
+
+exemplaires=(
+    5_100
+    10_10
+    10_20
+    10_40
+    20_5
+)
+
+for exemplaire in "${exemplaires[@]}"; do
+    check_exemplaire "$exemplaire"
+done
